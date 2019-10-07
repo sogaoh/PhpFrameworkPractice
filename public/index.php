85ea72b19248e01ab049d00ca4cb28153016ba75
sogaoh <sogaoh@ant-in-giant.work>
2019-10-07 20:14:35 +0900

    phpinfo.phpを削除して index.php だけで済むように対応

--- public/index.php
+++ public/index.php
@@ -2,7 +2,45 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-echo "<!DOCTYPE html>\n";
-echo "<title>test</title>\n";
-echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
-echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+$routes = [];
+
+$routes['/'] = function(){
+    echo "<!DOCTYPE html>\n";
+    echo "<title>test</title>\n";
+    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
+    echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+    echo "<hr>\n";
+    exit;
+};
+
+$routes['/phpinfo.php'] = function () {
+    phpinfo();
+};
+
+if (isset($routes[$_SERVER['REQUEST_URI']])) {
+    $f = $routes[$_SERVER['REQUEST_URI']];
+    $f();
+} else {
+    http_response_code(404);
+    echo '<h1>404 Not Found</h1>';
+}
+
+
+/*
+if ($_SERVER['REQUEST_URI'] === '/') {
+    echo "<!DOCTYPE html>\n";
+    echo "<title>test</title>\n";
+    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')) . "です</p>\n";
+    echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+    echo "<hr>\n";
+    exit;
+}
+
+if ($_SERVER['REQUEST_URI'] === '/phpinfo.php') {
+    phpinfo();
+    exit;
+}
+
+http_response_code(404);
+echo '<h1>404 Not Found</h1>';
+*/
